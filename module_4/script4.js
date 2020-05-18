2e3e45c3eae5848d04376469b4d242221a069852
shikshya720 <64352603+shikshya720@users.noreply.github.com>
2020-05-18 12:52:01 +0545

    Update script4.js

--- module_4/script4.js
+++ module_4/script4.js
@@ -1,5 +1,5 @@
 (function(window){
-	var names =["shikshya","Laxmi","Monika","John","Paul","anisha","jeshika","Julie"];
+	var names =["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 	for (var i=0; i < names.length; i++){
 		var firstLetter = names[i].charAt(0).toLowerCase();
 		if (firstLetter ==='j') {
